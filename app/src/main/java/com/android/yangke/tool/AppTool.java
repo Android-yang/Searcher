4057341ba1e06a8566cd383a42c94ce4991a21aa
yangkexue <yangkexue@swjtu-richsun.com>
2018-11-02 11:07:06 +0800

    新增打包自动获取闪屏时长；例：release 版本 3 秒，debug 版本 1 毫秒；

--- app/src/main/java/com/android/yangke/tool/AppTool.java
+++ app/src/main/java/com/android/yangke/tool/AppTool.java
@@ -1,5 +1,7 @@
 package com.android.yangke.tool;
 
+import com.android.yangke.BuildConfig;
+
 /**
  * author: yangke on 5/31/18.
  * weChat: ACE_5200125
@@ -7,6 +9,9 @@ package com.android.yangke.tool;
  * desc  :
  */
 public class AppTool {
+    public static final boolean ENVIRONMENT_RELEASE = BuildConfig.ENVIRONMENT; //打包环境
+
     //闪屏时长
-    public static final int SPLASH_SCREEN_DURATION = 3000;
+    public static final int SPLASH_SCREEN_DURATION = ENVIRONMENT_RELEASE == true ? 3000 : 1;
+
 }
